435eb0983250cf1fb1b26e53406e34ab0f08a2fe
weijun <asd101250101@gmail.com>
2025-02-16 18:09:34 +0800

    fix(table/menus/index.tsx): fix build log

--- components/tiptap/extensions/Table/menus/index.tsx
+++ components/tiptap/extensions/Table/menus/index.tsx
@@ -1,2 +1,6 @@
-export * from './TableColumn'
-export * from './TableRow'
+export { TableColumnMenu } from "./TableColumn";
+export { TableRowMenu } from "./TableRow";
+export type { MenuProps as TableColumnMenuProps } from "./TableColumn";
+export type { MenuProps as TableRowMenuProps } from "./TableRow";
+export type { ShouldShowProps as TableColumnShouldShowProps } from "./TableColumn";
+export type { ShouldShowProps as TableRowShouldShowProps } from "./TableRow";
