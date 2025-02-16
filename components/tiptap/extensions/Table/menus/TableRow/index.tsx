fcb7cb9cdfb61072478bd22827b436a3867fa478
weijun <asd101250101@gmail.com>
2025-02-16 18:05:43 +0800

    fix(tablerow/index.tsx): fix build log

--- components/tiptap/extensions/Table/menus/TableRow/index.tsx
+++ components/tiptap/extensions/Table/menus/TableRow/index.tsx
@@ -1,72 +1,96 @@
-import { BubbleMenu as BaseBubbleMenu } from '@tiptap/react'
-import React, { useCallback } from 'react'
-import * as PopoverMenu from '@/components/ui/PopoverMenu'
+import { BubbleMenu as BaseBubbleMenu } from "@tiptap/react";
+import React, { useCallback, ReactElement } from "react";
+import * as PopoverMenu from "@/components/ui/PopoverMenu";
 
-import { Toolbar } from '@/components/ui/Toolbar'
-import { isRowGripSelected } from './utils'
-import { Icon } from '@/components/ui/Icon'
-import { MenuProps, ShouldShowProps } from '@/components/menus/types'
+import { Toolbar } from "@/components/ui/Toolbar";
+import { isRowGripSelected } from "./utils";
+import { Icon } from "@/components/ui/Icon";
+import { Editor } from "@tiptap/react";
+import { Editor as CoreEditor } from "@tiptap/core";
+import { EditorState } from "@tiptap/pm/state";
+import { EditorView } from "@tiptap/pm/view";
+export interface MenuProps {
+  editor: Editor;
+  appendTo?: React.RefObject<HTMLElement>;
+  shouldHide?: boolean;
+}
 
-export const TableRowMenu = React.memo(({ editor, appendTo }: MenuProps): JSX.Element => {
-  const shouldShow = useCallback(
-    ({ view, state, from }: ShouldShowProps) => {
-      if (!state || !from) {
-        return false
-      }
+export interface ShouldShowProps {
+  editor?: CoreEditor;
+  view: EditorView;
+  state?: EditorState;
+  oldState?: EditorState;
+  from?: number;
+  to?: number;
+}
 
-      return isRowGripSelected({ editor, view, state, from })
-    },
-    [editor],
-  )
+export const TableRowMenu = React.memo(
+  ({ editor, appendTo }: MenuProps): ReactElement => {
+    const shouldShow = useCallback(
+      ({ view, state, from }: ShouldShowProps) => {
+        if (!state || !from) {
+          return false;
+        }
 
-  const onAddRowBefore = useCallback(() => {
-    editor.chain().focus().addRowBefore().run()
-  }, [editor])
+        return isRowGripSelected({ editor, view, state, from });
+      },
+      [editor]
+    );
 
-  const onAddRowAfter = useCallback(() => {
-    editor.chain().focus().addRowAfter().run()
-  }, [editor])
+    const onAddRowBefore = useCallback(() => {
+      editor.chain().focus().addRowBefore().run();
+    }, [editor]);
 
-  const onDeleteRow = useCallback(() => {
-    editor.chain().focus().deleteRow().run()
-  }, [editor])
+    const onAddRowAfter = useCallback(() => {
+      editor.chain().focus().addRowAfter().run();
+    }, [editor]);
 
-  return (
-    <BaseBubbleMenu
-      editor={editor}
-      pluginKey="tableRowMenu"
-      updateDelay={0}
-      tippyOptions={{
-        appendTo: () => {
-          return appendTo?.current
-        },
-        placement: 'left',
-        offset: [0, 15],
-        popperOptions: {
-          modifiers: [{ name: 'flip', enabled: false }],
-        },
-      }}
-      shouldShow={shouldShow}
-    >
-      <Toolbar.Wrapper isVertical>
-        <PopoverMenu.Item
-          iconComponent={<Icon name="ArrowUpToLine" />}
-          close={false}
-          label="Add row before"
-          onClick={onAddRowBefore}
-        />
-        <PopoverMenu.Item
-          iconComponent={<Icon name="ArrowDownToLine" />}
-          close={false}
-          label="Add row after"
-          onClick={onAddRowAfter}
-        />
-        <PopoverMenu.Item icon="Trash" close={false} label="Delete row" onClick={onDeleteRow} />
-      </Toolbar.Wrapper>
-    </BaseBubbleMenu>
-  )
-})
+    const onDeleteRow = useCallback(() => {
+      editor.chain().focus().deleteRow().run();
+    }, [editor]);
 
-TableRowMenu.displayName = 'TableRowMenu'
+    return (
+      <BaseBubbleMenu
+        editor={editor}
+        pluginKey="tableRowMenu"
+        updateDelay={0}
+        tippyOptions={{
+          appendTo: () => {
+            return appendTo?.current || document.body;
+          },
+          placement: "left",
+          offset: [0, 15],
+          popperOptions: {
+            modifiers: [{ name: "flip", enabled: false }],
+          },
+        }}
+        shouldShow={shouldShow}
+      >
+        <Toolbar.Wrapper isVertical>
+          <PopoverMenu.Item
+            iconComponent={<Icon name="ArrowUpToLine" />}
+            close={false}
+            label="Add row before"
+            onClick={onAddRowBefore}
+          />
+          <PopoverMenu.Item
+            iconComponent={<Icon name="ArrowDownToLine" />}
+            close={false}
+            label="Add row after"
+            onClick={onAddRowAfter}
+          />
+          <PopoverMenu.Item
+            icon="Trash"
+            close={false}
+            label="Delete row"
+            onClick={onDeleteRow}
+          />
+        </Toolbar.Wrapper>
+      </BaseBubbleMenu>
+    );
+  }
+);
 
-export default TableRowMenu
+TableRowMenu.displayName = "TableRowMenu";
+
+export default TableRowMenu;
