6fc7afaa975937ba482eb74cb05e7781075201ec
Weigun SU <codingprovide@gmail.com>
2025-02-19 19:34:09 +0800

    feat(ui): add link in dashborad page to wrokspace

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function DashBoardPage() {
   return (
     <div className="flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <p className="mt-2">Welcome to the dashboard!</p>
+      <Link href={"/dashboard/workspace"}>
+        <Button>Go to workspace</Button>
+      </Link>
     </div>
   );
 }
